eb687c1066939e30fbd57239f7e97a6f6e963005
Hang Jiayu <88521229+Hang-Jiayu@users.noreply.github.com>
2022-08-31 00:10:05 +0800

    RedStoneLauncherInterface | 完成下载优化B期

--- SendRequest.py
+++ SendRequest.py
@@ -30,6 +30,13 @@ if __name__ == '__main__':
     path=os.getcwd()+"\\"
     path="D:\\GolangFiles\\RedStoneGameCache\\"
 
+    respond=requests.post("http://127.0.0.1:30713/java_list")
+    print(respond.text)
+    respond=requests.post("http://127.0.0.1:30713/adduser_offline",{"UserName":"Tester"})
+    print(respond.text)
+    respond=requests.post("http://127.0.0.1:30713/Select_user",{"UserName":"Tester"})
+    print(respond.text)
+
     # respond=requests.post("http://127.0.0.1:30713/download",data={"MCVersion":"1.7.10","VersionName":"1.7.10","RetryTimes":5,"DownloadSource":2,"WorkPath":path})
     # respond=requests.post("http://127.0.0.1:30713/execute",data={"MCVersion":"1.7.10","VersionName":"1.7.10","UserName":"StarDream","RetryTimes":5,"DownloadSource":2,"JavaPath":"'D:\\JavaJDK\\bin\\java.exe' ","WorkPath":path})
     # respond=requests.post("http://127.0.0.1:30713/download",data={"MCVersion":"1.12.2","VersionName":"1.12.2","RetryTimes":10,"DownloadSource":2,"WorkPath":path})
@@ -38,14 +45,9 @@ if __name__ == '__main__':
     # respond=requests.post("http://127.0.0.1:30713/execute",data={"MCVersion":"1.14.4","VersionName":"1.14.4","UserName":"StarDream","RetryTimes":5,"DownloadSource":2,"JavaPath":"'D:\\JavaJDK\\bin\\java.exe' ","WorkPath":path})
     #respond=requests.post("http://127.0.0.1:30713/download",data={"MCVersion":"1.16","VersionName":"1.16","RetryTimes":5,"DownloadSource":2,"WorkPath":path})
     #respond=requests.post("http://127.0.0.1:30713/execute",data={"MCVersion":"1.16","VersionName":"1.16","UserName":"StarDream","RetryTimes":5,"DownloadSource":2,"JavaPath":"'D:\\JavaJDK\\bin\\java.exe' ","WorkPath":path})
-    #respond=requests.post("http://127.0.0.1:30713/download",data={"MCVersion":"1.18.2","VersionName":"1.18.2","RetryTimes":5,"DownloadSource":2,"WorkPath":path})
-    #respond=requests.post("http://127.0.0.1:30713/execute",data={"MCVersion":"1.18.2","VersionName":"1.18.2","UserName":"StarDream","RetryTimes":5,"DownloadSource":2,"JavaPath":"'D:\\JavaJDK\\bin\\java.exe' ","WorkPath":path})
+    respond=requests.post("http://127.0.0.1:30713/download",data={"MCVersion":"1.18.2","VersionName":"1.18.2","RetryTimes":5,"DownloadSource":2,"WorkPath":path})
+    respond=requests.post("http://127.0.0.1:30713/execute",data={"MCVersion":"1.18.2","VersionName":"1.18.2","UserName":"StarDream","RetryTimes":5,"DownloadSource":2,"JavaPath":"'D:\\JavaJDK\\bin\\java.exe' ","WorkPath":path})
     # respond=requests.post("http://127.0.0.1:30713/download",data={"MCVersion":"1.19.2","VersionName":"1.19.2","RetryTimes":5,"DownloadSource":2,"WorkPath":path})
     # respond=requests.post("http://127.0.0.1:30713/execute",data={"MCVersion":"1.19.2","VersionName":"1.19.2","UserName":"StarDream","RetryTimes":5,"DownloadSource":2,"JavaPath":"'D:\\JavaJDK\\bin\\java.exe' ","WorkPath":path})
-    respond=requests.post("http://127.0.0.1:30713/java_list")
-    print(respond.text)
-    respond=requests.post("http://127.0.0.1:30713/adduser_offline",{"UserName":"Tester"})
-    print(respond.text)
-    respond=requests.post("http://127.0.0.1:30713/Select_user",{"UserName":"Tester"})
-    print(respond.text)
+
     print("Divide---------------------------------------------------------------------\n\n\n")
